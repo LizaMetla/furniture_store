78d7f9280efaa84df124c1571a34e35acd4a0e30
Liza <liza.metla@gmail.com>
2019-03-05 12:24:45 +0300

    globally changed structures added menu functions

--- core.py
+++ core.py
@@ -1,44 +1,46 @@
 def get_worker():
-    worker = {
-        "id": None,
-        "first_n": None,
-        "last_n": None,
-        "middle_n": None,
-        "individual_position": None,
-        "tel_number": None,
-    }
-    return worker
-
-
-def get_product():
-    product = {
-        "id": None,
-        "department": None,
-        "name": None,
-        "product_price": None,
-    }
-    return product
-
-
-def get_workers_position():
-    workers_position = {
-        "position_id": None,
-        "position": None,
-        "salary": None,
+    department = {
+        "id_department": None, # идент. отдела
+        "department_name": None, # название отдела
+        "department_leader": None, # управляющий отделом
 
     }
-    return workers_position
+    return department
+
+
+def get_furniture():
+    furniture = {
+        "id_product": None,  # идент. продукта
+        "id_department": None, # идент. отдела
+        "birthday": None,  # дата создания продукта
+        "name": None, # название продукта
+        "product_price": None, # цена продукта
+    }
+    return furniture
+
+
+def get_batch(): # партия
+    batch = {
+        "batch_id": None, # идент. партии
+        "delivery_date": None, # дата привоза партии
+        "batch_number": None, # количество партии
+        "batch_price": None, # цена партии
+
+    }
+    return batch
 def menu():
     menu_info = '''
     1. Просмотр 
     2. Удаление
+    3. Добавление
+    4. Редактирование
     '''
 
 
 def main():
     menu()
-    get_product()
-    get_workers_position()
+    get_furniture()
+    get_batch()
 
 #заменить слово "партия товаров"
 #расписать функции пользователя(создать:1)партию 2) пользователя и тд)
