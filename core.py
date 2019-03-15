5fde8368ea2d73f4adb342f09c17c0a7afbef50b
Liza <liza.metla@gmail.com>
2019-03-15 14:35:38 +0300

    added dictionaries

--- core.py
+++ core.py
@@ -1,4 +1,5 @@
-from db_layer import *   # чтобы доставать информацию из файла,не перечисляя все ф-ии
+from db_layer import *  # чтобы доставать информацию из файла,не перечисляя все ф-ии
+
 
 # функции заполнения данными
 
@@ -50,14 +51,28 @@ def menu(menu_info):
         except ValueError:
             print('Повторите ввод!')
 
+# фунции поиска элемента для осуществления просмотра/редактирования/удаления
 
 def search_furniture(id_furniture):
-    furnitures = get_from_db_furniture()
-    for furniture in furnitures:
+    appointments = get_from_db_furniture()
+    for furniture in appointments:
         if furniture['id_furniture'] == id_furniture:
-           return furnitures
+            return appointments
 
-# something is clearly wrong, because i'm loaf :)
+
+def search_department(id_department):
+    departments = get_from_db_department()
+    for department in departments:
+        if department['id_department'] == id_department:
+            return departments
+
+
+def search_batch(id_batch):
+    batches = get_from_db_batch()
+    for batch in batches:
+        if batch['id_batch'] == id_batch:
+            return batches
+
 
 def main_menu():
     menu_info = '''
@@ -70,15 +85,52 @@ def main_menu():
     0. Выход.
     '''
     answer = menu(menu_info)
-    {1: '',
+    {1: filter,
      2: '',
      3: add_menu,
      4: '',
-     5: filter,
+     5: '',
      0: ''}.get(answer, "Ввод параметров неверный!")()
 
+# created new menu
 
-# added functions for all shop instance
+
+def view_menu():
+    view_menu_info = '''
+    Осуществить просмотр информации о продукте:
+    1. По ID Продукта  
+    2. По названию продукта 
+    3. Выйти в основное меню 
+    
+    0. Выход.
+    '''
+
+def delete_menu():
+    delete_menu_info = '''
+    Осуществить удаление по:
+    1. По ID продукта
+    2. По названию 
+    3. Выйти в основное меню 
+    
+    0. Выход.
+    '''
+
+def editing_menu():
+    editing_menu_info = '''
+    1. Введите конкретный продукт информацию о котором вы хотите изменить:
+    2. Выйти в основное меню 
+    
+    0. Выход.
+    '''
+# здесь нужно создать подменю , в котором мы выбираем какое поле изменить (?)
+# Возможно ли его создать в editing_menu ?
+
+
+def filter_by_id():
+    pass
+# это отдельная функция по фильтрации продуктов по id
+
+# функции по добавлению элементов
 
 
 def add_furniture():
@@ -114,6 +166,9 @@ def add_menu():
      0: ''}.get(answer, "Ввод параметров неверный!")()
 
 
+# функция для просмотра содержимого
+
+
 def filter(shop_instance_list, **kwargs):
     search_result = []
     for instance in shop_instance_list:
@@ -122,7 +177,6 @@ def filter(shop_instance_list, **kwargs):
                 if instance[param] == kwargs[param] and instance not in search_result:
                     search_result.append(instance)
     return search_result
-
 
 
 def main():
