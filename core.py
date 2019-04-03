685abb63ddd01a62b6d620ecd4523c5933dc6be1
Liza <liza.metla@gmail.com>
2019-04-03 10:10:33 +0300

    db changed +(need to add sort func) //don't delete information in file

--- core.py
+++ core.py
@@ -4,7 +4,7 @@ from menu_info import main_menu_info, search_choice, add_menu_info
 
 
 def menu_input(menu_info):
-    while 1:
+    while True:
         print(menu_info)
         answer = input('Выберите пункт меню: ')
         try:
@@ -59,8 +59,8 @@ def add_menu():
                   2: get_from_db_department,
                   3: get_from_db_batch}.get(answer, send_error)()
     empty_db_obj = {1: get_furniture,
-                    2: get_batch,
-                    3: get_department}.get(answer, send_error)()
+                    2: get_department,
+                    3: get_batch}.get(answer, send_error)()
     fill_shop_instance(empty_db_obj, db_objects['db_name'])
     add_to_db(db_objects, empty_db_obj)
 
@@ -69,11 +69,19 @@ def add_menu():
 
 def fill_shop_instance(data, db_name):
     for key in data:
-        if db_name == "Furniture.pickle" and key == "id_batch":
-            answer = None
-            while answer is None:
-                pass #TODO
-        data[key] = input(f'{key}: ')
+        value = input(f'{key}: ')
+        if db_name == "Furniture.pickle":
+            if key == "id_batch":
+                while not filter_(get_from_db_batch(), id_batch=value):
+                    print('Поставка с данным id ещё не создана.', 'Введите id снова - ' )
+                    value = input(f'{key}: ')
+
+            elif key == "id_department":
+                while not filter_(get_from_db_department(), id_department=value):
+                    print('Отдел с данным id ещё не создан.', 'Введите id снова - ' )
+                    value = input(f'{key}: ')
+
+        data[key] = value
 
 
 def edit_db_obj_menu():
@@ -110,17 +118,18 @@ def send_error(*args, **kwargs):
     return {}
 
 
+# функция для просмотра содержимого
+
+
+def filter_(shop_instance_list, **kwargs):
+    search_result = []
+    for instance in shop_instance_list['db_objects']:
+        if all(param in instance for param in kwargs):
+            for param in kwargs:
+                if instance[param] == kwargs[param] and instance not in search_result:
+                    search_result.append(instance)
+    return search_result
+
 if __name__ == '__main__':
     main()
 
-# функция для просмотра содержимого
-#
-#
-# def filter_(shop_instance_list, **kwargs):
-#     search_result = []
-#     for instance in shop_instance_list:
-#         if all(param in instance for param in kwargs):
-#             for param in kwargs:
-#                 if instance[param] == kwargs[param] and instance not in search_result:
-#                     search_result.append(instance)
-#     return search_result
